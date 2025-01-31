7e212beb93440bc802ccda0fa7b1da5196c56e0c
cel004 <ngcelinaa@gmail.com>
2025-01-31 16:57:30 -0500

    Add animation to answers and ball

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 function shake8Ball() {
     const frontBall = document.getElementById('front');
+    const blankBall = document.getElementById('blank');
     
+    frontBall.style.opacity = 1;
+    blankBall.style.animation = 'shake 1s ease-in-out';
     frontBall.style.transition = 'opacity 0.4s';
     frontBall.style.opacity = 0;
+
+    setTimeout(() => {
+        blankBall.style.animation = '';
+        fadeInRandomImage();
+    }, 1000);
+}
+
+function fadeInRandomImage() {
+    const images = [
+        '/assets/answers/HELLYEAH.png',
+        '/assets/answers/IGUESS.png',
+        '/assets/answers/ITHINKNOT.png',
+        '/assets/answers/MAYBE.png',
+        '/assets/answers/NO.png',
+        '/assets/answers/NOOOOO.png',
+        '/assets/answers/ONLYYOUCANKNOWFORSURE.png',
+        '/assets/answers/SHAKEAGAIN.png',
+        '/assets/answers/UHHHH.png',
+        '/assets/answers/WHATTHEHELLSURE.png',
+        '/assets/answers/YES.png',
+    ];
+
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+
+    const frontBall = document.getElementById('front');
+    frontBall.src = randomImage;
+    frontBall.style.opacity = 0;
+    frontBall.style.paddingTop = '0.7rem';
+    frontBall.style.width = '25vw';
+
+    setTimeout(() => {
+        frontBall.style.transition = 'opacity 1s';
+        frontBall.style.opacity = 1;
+    }, 100);
 }
